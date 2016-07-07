7ea166f95823583d18b5e7931e0632880167d7bf
Richard van Heest <richard.v.heest@gmail.com>
2016-07-07 21:40:05 +0200

    StateT as a generalization of Parser

---
+++ Reader/src/main/scala/nl/rvanheest/sheetmusicreader/monadics/Structures.scala
@@ -0,0 +1,101 @@
+package nl.rvanheest.sheetmusicreader.monadics
+
+import scala.language.higherKinds
+
+trait Functor[F[_]] {
+	def map[A, B](functor: F[A])(f: A => B): F[B]
+}
+
+trait Applicative[App[_]] extends Functor[App] {
+	def create[A](a: A): App[A]
+
+	def <*>[A, B](appFunc: App[A => B], appA: App[A]): App[B]
+
+	def *>[A, B](appA: App[A], appB: App[B]): App[B] = {
+		<*>(map[A, B => B](appA)(_ => identity[B]), appB)
+	}
+
+	def <*[A, B](appA: App[A], appB: App[B]): App[A] = {
+		<*>(map[A, B => A](appA)(a => _ => a), appB)
+	}
+
+	def <**>[A, B](appA: App[A], appFunc: App[A => B]): App[B] = {
+		<*>(appFunc, appA)
+	}
+}
+
+trait Monad[M[_]] extends Applicative[M] {
+	override def <*>[A, B](mFunc: M[A => B], mA: M[A]): M[B] = {
+		flatMap(mA)(a => map(mFunc)(_(a)))
+	}
+
+	override def *>[A, B](mA: M[A], mB: M[B]): M[B] = {
+		andThen(mA, mB)
+	}
+
+	override def <*[A, B](mA: M[A], mB: M[B]): M[A] = {
+		thenAnd(mA, mB)
+	}
+
+	def flatMap[A, B](monad: M[A])(f: A => M[B]): M[B]
+
+	def andThen[A, B](mA: M[A], mB: M[B]): M[B] = {
+		flatMap(mA)(a => mB)
+	}
+
+	def thenAnd[A, B](mA: M[A], mB: M[B]): M[A] = {
+		flatMap(mA)(a => map(mB)(_ => a))
+	}
+
+	def flatten[A, B](mA: M[A])(implicit ev: A <:< M[B]): M[B] = {
+		flatMap(mA)(ev)
+	}
+}
+
+trait Alternative[Alt[_]] extends Applicative[Alt] {
+	def empty[A]: Alt[A]
+
+	def orElse[A, B >: A](alt1: Alt[A], alt2: => Alt[B]): Alt[B]
+
+	def atLeastOnce[A](alt: Alt[A]): Alt[List[A]] = {
+		atLeastOnce_v(alt)
+	}
+
+	def many[A](alt: Alt[A]): Alt[List[A]] = {
+		many_v(alt)
+	}
+
+	private def many_v[A](alt: Alt[A]): Alt[List[A]] = {
+		orElse(atLeastOnce_v(alt), create(Nil))
+	}
+
+	private def atLeastOnce_v[A](alt: Alt[A]): Alt[List[A]] = {
+		<*>(map[A, List[A] => List[A]](alt)(a => a :: _), many_v(alt))
+	}
+
+	def maybe[A](alt: Alt[A]): Alt[Option[A]] = {
+		orElse(map(alt)(Some(_)), create(None))
+	}
+}
+
+trait MonadPlus[MP[_]] extends Monad[MP] with Alternative[MP] {
+	def mplus[A, B >: A](mp1: MP[A], mp2: MP[B]): MP[B] = {
+		orElse(mp1, mp2)
+	}
+
+	def filter[A](mp: MP[A])(predicate: A => Boolean): MP[A] = {
+		flatMap(mp)(a => if (predicate(a)) create(a) else empty[A])
+	}
+
+	def filterNot[A](mp: MP[A])(predicate: A => Boolean): MP[A] = {
+		filter(mp)(!predicate(_))
+	}
+
+	def takeUntil[A](mp: MP[A])(predicate: A => Boolean): MP[List[A]] = {
+		many(filterNot(mp)(predicate))
+	}
+
+	def takeWhile[A](mp: MP[A])(predicate: A => Boolean): MP[List[A]] = {
+		many(filter(mp)(predicate))
+	}
+}
---
+++ Reader/src/main/scala/nl/rvanheest/sheetmusicreader/monadics/package.scala
@@ -0,0 +1,42 @@
+package nl.rvanheest.sheetmusicreader
+
+import scala.util.{Failure, Success, Try}
+
+package object monadics {
+
+	implicit object OptionIsMonadPlus extends MonadPlus[Option] {
+		def empty[A]: Option[A] = None
+
+		def create[A](a: A): Option[A] = Some(a)
+
+		def orElse[A, B >: A](alt1: Option[A], alt2: => Option[B]): Option[B] = {
+			alt1.orElse(alt2)
+		}
+
+		def map[A, B](option: Option[A])(f: A => B): Option[B] = {
+			option.map(f)
+		}
+
+		def flatMap[A, B](option: Option[A])(f: A => Option[B]): Option[B] = {
+			option.flatMap(f)
+		}
+	}
+
+	implicit object TryIsMonadPlus extends MonadPlus[Try] {
+		def empty[A]: Try[A] = Failure(new NoSuchElementException("empty"))
+
+		def create[A](a: A): Try[A] = Success(a)
+
+		def orElse[A, B >: A](self: Try[A], other: => Try[B]): Try[B] = {
+			self.orElse(other)
+		}
+
+		def map[A, B](self: Try[A])(f: A => B): Try[B] = {
+			self.map(f)
+		}
+
+		def flatMap[A, B](self: Try[A])(f: A => Try[B]): Try[B] = {
+			self.flatMap(f)
+		}
+	}
+}
